2bf81b783d978fe0186096fba8ec577daa2adbbe
Victor Pakhomov <1146089+fossil@users.noreply.github.com>
2020-11-20 00:36:52 +0100

    added auto scale group

--- resource.tf
+++ resource.tf
@@ -1,16 +1,30 @@
-resource "aws_instance" "example" {
-  ami                    = "ami-0c55b159cbfafe1f0"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id]
-
+resource "aws_launch_configuration" "example" {
+  image_id        = "ami-0c55b159cbfafe1f0"
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.instance.id]
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
               nohup busybox httpd -f -p "${var.server_port}" &
               EOF
+  lifecycle {
+    create_before_destroy = true
+  }
+}
 
-  tags = {
-    Name = "terraform-example"
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.id
+  availability_zones   = data.aws_availability_zones.all.names
+  min_size = 2
+  max_size = 10
+
+  load_balancers    = [aws_elb.example.name]
+  health_check_type = "ELB"
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
   }
 }
 
@@ -23,3 +37,44 @@ resource "aws_security_group" "instance" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+resource "aws_elb" "example" {
+  name               = "terraform-asg-example"
+  security_groups    = [aws_security_group.elb.id]
+  availability_zones = data.aws_availability_zones.all.names
+
+  health_check {
+    target              = "HTTP:${var.server_port}/"
+    interval            = 30
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+
+  # This adds a listener for incoming HTTP requests.
+  listener {
+    lb_port           = 80
+    lb_protocol       = "http"
+    instance_port     = var.server_port
+    instance_protocol = "http"
+  }
+}
+
+resource "aws_security_group" "elb" {
+  name = "terraform-example-elb"
+  # Allow all outbound
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  # Inbound HTTP from anywhere
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
