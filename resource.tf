d67c3731235730c0bdf870d91021b1d2bca02a40
Victor Pakhomov <1146089+fossil@users.noreply.github.com>
2020-11-25 13:28:37 +0100

    added general syntax comment

--- resource.tf
+++ resource.tf
@@ -1,3 +1,9 @@
+# General syntax for Terraform resource:
+#
+# resource "<PROVIDER>_<TYPE>" "<NAME>" {
+# [CONFIG …]
+# }
+#
 resource "aws_instance" "example" {
   ami           = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
